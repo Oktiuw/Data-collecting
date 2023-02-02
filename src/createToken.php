14c84f273624d2b91a16c7c616d16ceda816775f
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-02-02 12:59:06 +0100

    ajout cURL et test d'un nv scrpit pour le token

--- src/createToken.php
+++ src/createToken.php
@@ -1,31 +1,47 @@
 <?php
-$clientId = 'PAR_analysedata_120bb0d3ffce53c90a8e133832e7d9b4431b76f625e5e71cbba39e594f831375';
-$clientSecret = '913cee3ab7181d067d794cdcfb9e82762a6562b93540e897ff795825f8da0f70';
 
-$headers = array(
-    'Content-Type: application/x-www-form-urlencoded',
-    'Authorization: Basic '.base64_encode($clientId.':'.$clientSecret)
+$clientId = "PAR_analysedata_120bb0d3ffce53c90a8e133832e7d9b4431b76f625e5e71cbba39e594f831375";
+$clientSecret = "913cee3ab7181d067d794cdcfb9e82762a6562b93540e897ff795825f8da0f70";
+$scopes = "api_stats-offres-demandes-emploiv1 offresetdemandesemploi";
+
+// Définir l'URL pour obtenir le jeton d'authentification
+$tokenUrl = "https://api.emploi-store.fr/partenaire/token";
+
+// Paramètres de la demande
+$params = array(
+    "grant_type" => "client_credentials",
+    "client_id" => $clientId,
+    "client_secret" => $clientSecret,
+    "scope" => $scopes
 );
 
-$postData = 'grant_type=client_credentials';
-
+// Initialiser cURL
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https:/pole-emploi.io/data/api/marche-travail');
+
+// Configurer cURL pour la demande POST
+curl_setopt($ch, CURLOPT_URL, $tokenUrl);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-$result = curl_exec($ch);
-$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+// Exécuter la demande et récupérer la réponse
+$response = curl_exec($ch);
 
+// Décoder la réponse JSON
+$responseData = json_decode($response, true);
+
+// Vérifier si la demande a réussi
+if (curl_errno($ch)) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    // Récupérer le jeton d'authentification
+    $accessToken = $responseData["access_token"];
+
+    // Utiliser le jeton pour les demandes futures
+    // ...
+}
+
+// Fermer cURL
 curl_close($ch);
 
-if ($httpCode >= 200 && $httpCode < 300) {
-    $result = json_decode($result, true);
-    $accessToken = $result['access_token'];
-    echo 'Access token: '.$accessToken;
-} else {
-    echo 'Error: '.$httpCode;
-}
 ?>
