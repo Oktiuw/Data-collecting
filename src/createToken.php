65bf77aea680b04b864a146e8d4780859016a743
Aurelien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-02-02 14:04:35 +0100

    script access token OK

--- src/createToken.php
+++ src/createToken.php
@@ -1,47 +1,35 @@
 <?php
-
 $clientId = "PAR_analysedata_120bb0d3ffce53c90a8e133832e7d9b4431b76f625e5e71cbba39e594f831375";
 $clientSecret = "913cee3ab7181d067d794cdcfb9e82762a6562b93540e897ff795825f8da0f70";
-$scopes = "api_stats-offres-demandes-emploiv1 offresetdemandesemploi";
+$scope = "api_stats-offres-demandes-emploiv1 offresetdemandesemploi";
 
-// Définir l'URL pour obtenir le jeton d'authentification
-$tokenUrl = "https://api.emploi-store.fr/partenaire/token";
+$curl = curl_init();
 
-// Paramètres de la demande
-$params = array(
-    "grant_type" => "client_credentials",
-    "client_id" => $clientId,
-    "client_secret" => $clientSecret,
-    "scope" => $scopes
-);
-
-// Initialiser cURL
-$ch = curl_init();
-
-// Configurer cURL pour la demande POST
-curl_setopt($ch, CURLOPT_URL, $tokenUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Exécuter la demande et récupérer la réponse
-$response = curl_exec($ch);
-
-// Décoder la réponse JSON
-$responseData = json_decode($response, true);
-
-// Vérifier si la demande a réussi
-if (curl_errno($ch)) {
-    echo 'Error: ' . curl_error($ch);
-} else {
-    // Récupérer le jeton d'authentification
-    $accessToken = $responseData["access_token"];
-
-    // Utiliser le jeton pour les demandes futures
-    // ...
+curl_setopt_array($curl, array(
+    CURLOPT_URL => "https://entreprise.pole-emploi.fr/connexion/oauth2/access_token?realm=/partenaire",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_SSL_VERIFYPEER => false,
+    CURLOPT_POSTFIELDS => "grant_type=client_credentials&client_id=$clientId&client_secret=$clientSecret&scope=$scope",
+    CURLOPT_HTTPHEADER => array(
+        "Content-Type: application/x-www-form-urlencoded"
+    ),
+));
+$response = curl_exec($curl);
+if (!$response) {
+    echo curl_error($curl);
 }
+curl_close($curl);
 
-// Fermer cURL
-curl_close($ch);
+var_dump($response);
+$response = json_decode($response, true);
 
+
+
+// Utilisez le jeton d'accès pour effectuer d'autres requêtes avec l'API
 ?>
