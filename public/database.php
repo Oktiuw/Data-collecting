c0bd21801f8027a34c3e18d8e0c4ce09accfd080
Aurélien Vincent <aurelien.vincent@etudiants.univ-reims.fr>
2023-02-10 15:54:15 +0100

    nouvelle gestion bd

--- public/database.php
+++ public/database.php
@@ -6,38 +6,23 @@ require_once 'vendor/autoload.php';
 
 $stmt1 = MyPDO::getInstance()->prepare(
     <<<'SQL'
-DROP TABLE IF EXISTS JobIndic;
-DROP TABLE IF EXISTS Activite ;
-DROP TABLE IF EXISTS TypeActivite;
+
+DROP TABLE IF EXISTS TypeTerritoire;
 DROP TABLE IF EXISTS Territoire;
+CREATE TABLE TypeTerritoire(
+    codeTypeTerritoire VARCHAR(20) PRIMARY KEY,
+    libelleTypeTerritoire VARCHAR(200)
+);
 CREATE TABLE Territoire(
-    codeTerritoire VARCHAR(3) PRIMARY KEY,
-    libelleTerritoire VARCHAR(100)
+    codeTerritoire VARCHAR(3),
+    libelleTerritoire VARCHAR(100),
+    codeTypeTerritoire VARCHAR(20),
+    PRIMARY KEY (codeTerritoire,codeTypeTerritoire),
+    FOREIGN KEY (codeTypeTerritoire) REFERENCES TypeTerritoire(codeTypeTerritoire)
 );
+INSERT INTO TypeTerritoire VALUES ('REG','Région française');
+INSERT INTO TypeTerritoire VALUES ('DEP','Département français');
 
-
-CREATE TABLE TypeActivite(
-    codeTpActivite VARCHAR(30) PRIMARY KEY,
-    libelleTpActivite VARCHAR(100)
-);
-CREATE TABLE Activite(
-    codeActivite VARCHAR(30),
-    libelleActivite VARCHAR(300),
-    codeTpActivite VARCHAR(30),
-    CONSTRAINT PK_Activite PRIMARY KEY (codeActivite, codeTpActivite),
-    CONSTRAINT FK_Activite_TypeActivite FOREIGN KEY (codeTpActivite)
-    REFERENCES TypeActivite(codeTpActivite)
-);
-
-CREATE TABLE JobIndic(
-    codeTerritore VARCHAR(3),
-    codeActivite VARCHAR(100),
-    libelleIndicateur VARCHAR(300),
-    valeurIndicateur FLOAT,
-    CONSTRAINT PK_JobIndic PRIMARY KEY (codeActivite,codeTerritore),
-    CONSTRAINT FK_JobIndic_Activite FOREIGN KEY (codeActivite) REFERENCES Activite(codeActivite),
-    CONSTRAINT FK_JobIndic_Territoire FOREIGN KEY (codeTerritore) REFERENCES Territoire(codeTerritoire)
-)
 SQL
 );
 
