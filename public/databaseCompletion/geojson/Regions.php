d4ad6385fc6ae877f818e4ce76e63ca9413513f1
Aurélien VINCENT <aurelien.vincent@etudiant.univ-reims.fr>
2023-03-14 21:23:14 +0100

    Ajout geojson DOM-TOM pour les dep

--- public/databaseCompletion/geojson/Regions.php
+++ public/databaseCompletion/geojson/Regions.php
@@ -7,6 +7,7 @@ $url = 'https://france-geojson.gregoiredavid.fr/repo/regions.geojson';
 $data = file_get_contents($url);
 $data = json_decode($data,true)['features'];
 foreach ($data as $r) {
+    $cdP=$r['properties']['code'];
     if (is_array($r)){
         $stmt = MyPDO::getInstance()->prepare(
             <<<'SQL'
@@ -15,6 +16,61 @@ foreach ($data as $r) {
     WHERE codeTerritoire=:cdP and codeTypeTerritoire='REG'
     SQL
         );
-        $stmt->execute([":cdP"=>$r['properties']['code'],':nb0'=>json_encode($r)]);
+        $stmt->execute([":cdP"=>$cdP,':nb0'=>json_encode($r)]);
+    }
+    if ($cdP==='01')
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    UPDATE Territoire
+    SET geoJson=:nb0
+    WHERE codeTerritoire=:cdP and codeTypeTerritoire='DEP'
+    SQL
+        );
+        $stmt->execute([":cdP"=>'971',':nb0'=>json_encode($r)]);
+    }
+    if ($cdP==='02')
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    UPDATE Territoire
+    SET geoJson=:nb0
+    WHERE codeTerritoire=:cdP and codeTypeTerritoire='DEP'
+    SQL
+        );
+        $stmt->execute([":cdP"=>'972',':nb0'=>json_encode($r)]);
+    }
+    if ($cdP==='03')
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    UPDATE Territoire
+    SET geoJson=:nb0
+    WHERE codeTerritoire=:cdP and codeTypeTerritoire='DEP'
+    SQL
+        );
+        $stmt->execute([":cdP"=>'973',':nb0'=>json_encode($r)]);
+    }
+    if ($cdP==='04')
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    UPDATE Territoire
+    SET geoJson=:nb0
+    WHERE codeTerritoire=:cdP and codeTypeTerritoire='DEP'
+    SQL
+        );
+        $stmt->execute([":cdP"=>'974',':nb0'=>json_encode($r)]);
+    }
+    if ($cdP==='06')
+    {
+        $stmt = MyPDO::getInstance()->prepare(
+            <<<'SQL'
+    UPDATE Territoire
+    SET geoJson=:nb0
+    WHERE codeTerritoire=:cdP and codeTypeTerritoire='DEP'
+    SQL
+        );
+        $stmt->execute([":cdP"=>'976',':nb0'=>json_encode($r)]);
     }
 }
